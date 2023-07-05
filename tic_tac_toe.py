923c0f595fcb33bc076559a269e5dc1a30c68fd6
mustanska <122823838+mustanska@users.noreply.github.com>
2023-07-05 20:16:09 +0300

    Added functionality to restart the game

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,5 +1,15 @@
-from logic_functions import draw_board
-from variables import board_position
+from variables import board_positions, board
+from logic_functions import draw_board, start_game
 
 print("These are the board position numbers you can choose from:")
-draw_board(board_position)
+draw_board(board_positions)
+
+start = input('For start press "Y", For leave press anything else: ').lower()
+
+while start == "y":
+    start_game(board)
+
+    start = input('Do you want to RESTART the game (press "Y" for restart or anything else to leave)? ').lower()
+else:
+    print("Goodbye!")
+
