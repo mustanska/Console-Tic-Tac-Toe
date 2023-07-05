9e9ec9bdb955ce34b6bda9f91b8cea0aee8257ce
mustanska <122823838+mustanska@users.noreply.github.com>
2023-07-05 20:14:40 +0300

    Added two more functions for validations

--- validation_functions.py
+++ validation_functions.py
@@ -15,6 +15,25 @@ def is_valid_symbol(current_input, symbols):
     return current_input in symbols
 
 
+# A function that check if position is valid
+def is_valid_position(current_position, positions):
+    for row in range(len(positions)):
+        if current_position in positions[row]:
+            return True
+
+    print(Fore.RED + "The position is not valid. You missed your turn..." + Fore.RESET)
+    return False
+
+
+# A function that checks for empty space in field
+def is_empty_spaces(field):
+    for row in range(len(field)):
+        for col in range(len(field[row])):
+            if field[row][col] == " ":
+                return True
+    return False
+
+
 # A function that checks for a unique player name and sign
 def is_unique_value(current_value, current_list):
     return current_value not in current_list
